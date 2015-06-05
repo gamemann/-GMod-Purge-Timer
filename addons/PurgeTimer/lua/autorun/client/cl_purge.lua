5478219f6753a9a9e50fcbb5dcd77421a2e69bef
gamemann <gamemann@gflclan.com>
2015-06-05 19:55:56 -0400

    Release 1.1
    Release 1.1:
    - Added Debugging

--- addons/PurgeTimer/lua/autorun/client/cl_purge.lua
+++ addons/PurgeTimer/lua/autorun/client/cl_purge.lua
@@ -1,8 +1,3 @@
-local ttimer = nil
-local etimer = nil
-
-local bPurgeInProgress = false
-
 surface.CreateFont( "FeastOfFleshA", {
 	font = "Feast Of Flesh BB",
 	size = 90,
@@ -17,19 +12,24 @@ surface.CreateFont( "FeastOfFleshB", {
 	antialias = true,
 })
 
-net.Receive("PurgeTimer",function()
-	ttimer = net.ReadUInt(32)
+-- Global Variables
+local tTimer = nil
+local eTimer = nil
+local bPurgeInProgress = false
+
+net.Receive("PurgeTimer", function()
+	tTimer = net.ReadUInt(32)
 	bPurgeInProgress = false
 end)
 
 net.Receive("PurgeEndTimer",function()
-	etimer = net.ReadUInt(32)
+	eTimer = net.ReadUInt(32)
 	bPurgeInProgress = true
 end)
 
 usermessage.Hook("PurgeStart", function(umsg)
 	if (IsValid(station)) then station:Play() end
-	chat.AddText(Color(200,72,72),"[ALERT] ",Color(255,255,255),"The Purge has begun! All crime is legal for 12 minutes! Good luck!")
+	chat.AddText(Color(200,72,72),"[ALERT]",Color(255,255,255),"The Purge has begun! All crime is legal for 12 minutes! Good luck!")
 	bPurgeInProgress = true
 end)
 
@@ -41,30 +41,31 @@ usermessage.Hook("LightStyleSet", function(umsg)
 end)
 
 usermessage.Hook("PurgeOver", function(umsg)
-	chat.AddText(Color(200,72,72),"[ALERT] ",Color(255,255,255),"The Purge has ended! Congratulations to everybody that survived!")
+	chat.AddText(Color(200,72,72),"[ALERT]",Color(255,255,255),"The Purge has ended! Congratulations to everybody that survived!")
 	bPurgeInProgress = false
 end)
 
 hook.Add("HUDPaint","PurgePaint",function()
-	if(ttimer) then
-		local tleft = ttimer-CurTime()
-		if(tleft <= 0 or bPurgeInProgress) then
-			ttimer = nil
+	if (tTimer) then
+		local tLeft = tTimer - CurTime()
+		if(tLeft <= 0 or bPurgeInProgress) then
+			tTimer = nil
 			return
 		end
-		if (ttimer) then
+		
+		if (tTimer) then
 			draw.SimpleText("THE PURGE BEGINS IN:","FeastOfFleshB",ScrW()/2,30,Color(200,7,7,255),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
-			draw.SimpleText(string.FormattedTime(tleft,"%02i:%02i"),"FeastOfFleshA",ScrW()/2,110,Color(200,7,7,255),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+			draw.SimpleText(string.FormattedTime(tLeft,"%02i:%02i"),"FeastOfFleshA",ScrW()/2,110,Color(200,7,7,255),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
 		end
-	elseif (etimer) then
-		local eleft = etimer-CurTime()
-		if(eleft <= 0 or not bPurgeInProgress) then
-			etimer = nil
+	elseif (eTimer) then
+		local eLeft = eTimer - CurTime()
+		if(eLeft <= 0 or not bPurgeInProgress) then
+			eTimer = nil
 			return
 		end
-		if (etimer) then
+		if (eTimer) then
 			draw.SimpleText("THE PURGE ENDS IN:","FeastOfFleshB",ScrW()/2,30,Color(122,0,0,255),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
-			draw.SimpleText(string.FormattedTime(eleft,"%02i:%02i"),"FeastOfFleshA",ScrW()/2,110,Color(122,0,0,255),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+			draw.SimpleText(string.FormattedTime(eLeft,"%02i:%02i"),"FeastOfFleshA",ScrW()/2,110,Color(122,0,0,255),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
 		end
 	end
 end)
--- addons/PurgeTimer/lua/autorun/server/sv_purge.lua
+++ addons/PurgeTimer/lua/autorun/server/sv_purge.lua
@@ -1,25 +1,29 @@
-PURGE = {}
+PURGE = {};
 
 -- Networking
-util.AddNetworkString("PurgeTimer")
-util.AddNetworkString("PurgeEndTimer")
+util.AddNetworkString("PurgeTimer");
+util.AddNetworkString("PurgeEndTimer");
 
 -- Global Variables
-local bTakeDamage = false
-local iRounds = 0
-local bPurgeInProgress = false
+local bTakeDamage = false;
+local iRounds = 0;
+local bPurgeInProgress = false;
 
 -- ConVars/Settings
 local pt1 = CreateConVar("sv_purge_time", "12", _, "The amount of time the Purge lasts for in minutes.");
 local pt2 = CreateConVar("sv_purge_setup_time", "45", _, "The amount of time until the Purge begins (setup time) in minutes.");
 local pt3 = CreateConVar("sv_purge_announcement_time", "52", _, "The amount of time the announcement before the Purge begins takes (length of the file) in seconds");
 local pt4 = CreateConVar("sv_purge_maxrounds", "16", _, "The maximum rounds (Purge beginning to end) until the map restarts to clear the precache table");
+local pt5 = CreateConVar("sv_purge_debug", "1", _, "Debug the Purge Timer?");
+local pt6 = CreateConVar("sv_purge_check", "1", _, "Check to ensure the Purge timer is set every 30 seconds?");
 
-local settings = {}
+local settings = {};
 settings.fPurgeTime = pt1:GetFloat();
 settings.fPurgeSetupTime = pt2:GetFloat();
 settings.fPurgeAnnouncementTime = pt3:GetFloat();
 settings.iPurgeMaxRounds = pt4:GetInt();
+settings.bDebug = pt5:GetBool();
+settings.bCheck = pt6:GetBool();
 
 -- Settings not with ConVars (feel free to edit)
 settings.arrWhiteList = 
@@ -30,22 +34,46 @@ settings.arrWhiteList =
 };
 
 cvars.AddChangeCallback("sv_purge_time", function (sCVarName, sOldv, sNewv)
+	if (settings.bDebug) then
+		print("[PT DEBUG]sv_purge_time set to " .. sNewv .. "\n");
+	end
+	
 	settings.fPurgeTime = pt1:GetFloat();
-end)
+end);
 
 cvars.AddChangeCallback("sv_purge_setup_time", function (sCVarName, sOldv, sNewv)
+	if (settings.bDebug) then
+		print("[PT DEBUG]sv_purge_setup_time set to " .. sNewv .. "\n");
+	end
+	
 	settings.fPurgeSetupTime = pt2:GetFloat();
-end)
+end);
 
 cvars.AddChangeCallback("sv_purge_announcement_time", function (sCVarName, sOldv, sNewv)
+	if (settings.bDebug) then
+		print("[PT DEBUG]sv_purge_announcement_time set to " .. sNewv .. "\n");
+	end
+	
 	settings.fPurgeAnnouncementTime = pt3:GetFloat();
-end)
+end);
 
 cvars.AddChangeCallback("sv_purge_maxrounds", function (sCVarName, sOldv, sNewv)
+	if (settings.bDebug) then
+		print("[PT DEBUG]sv_purge_maxrounds set to " .. sNewv .. "\n");
+	end
+	
 	settings.iPurgeMaxRounds = pt4:GetInt();
-end)
+end);
 
--- Verify Users
+cvars.AddChangeCallback("sv_purge_check", function (sCVarName, sOldv, sNewv)
+	if (settings.bDebug) then
+		print("[PT DEBUG]sv_purge_check set to " .. sNewv .. "\n");
+	end
+	
+	settings.bCheck = pt6:GetBool();
+end);
+
+-- Verify User
 local function VerifyUser (ply)
 	local bAllow = false;
 	for _,v in pairs (settings.arrWhiteList) do
@@ -56,203 +84,320 @@ local function VerifyUser (ply)
 	
 	return bAllow;
 end
-
--- Timers
-local function SetPurgeTimer()
-	PURGE.SetNight(false)
-	bTakeDamage = false
-	PURGE.Timer = CurTime()+settings.fPurgeSetupTime*60
-	net.Start("PurgeTimer")
-	net.WriteUInt(PURGE.Timer,32)
-	net.Broadcast()
-	BroadcastLua([[chat.AddText(Color(200,72,72),"[ALERT] ",Color(255,255,255),"The Purge starts in 45 minutes!")]])
-	bPurgeInProgress = false
+-- PURGE Functions
+function PURGE.SetNight(night)
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function PURGE.SetNight() :: Called.\n");
+	end
+	if(night) then
+		if (settings.bDebug) then
+			print("[PT DEBUG]Setting Night :: True.\n");
+		end
+		local lightenvs = ents.FindByClass("light_environment");
+		for _, light in pairs(lightenvs) do
+			if (IsValid(light)) then
+				 light:Fire( 'FadeToPattern' , 'a' , 0 ); -- z = brightest.
+			end
+		end
+	else
+		if (settings.bDebug) then
+			print("[PT DEBUG]Setting Night :: False.\n");
+		end
+		local lightenvs = ents.FindByClass("light_environment")
+		for _, light in pairs(lightenvs) do
+			if (IsValid(light)) then
+				 light:Fire( 'FadeToPattern' , 'v' , 0 ); -- z = brightest.
+			end
+		end
+	end
 end
 
-local function PurgeStart()
-	umsg.Start("PurgeStart")
-	umsg.End()
-	PURGE.SetNight(true)
-	PURGE.Timer = nil
-	bTakeDamage = true
-	bPurgeInProgress = true
+-- Local Functions
+local function KillTimers()
+	timer.Stop("Timer_SetPurgeTimer");
+	timer.Stop("Timer_PurgeAnnouncement");
+	timer.Stop("Timer_PurgeStart");
+	timer.Stop("Timer_PurgeOver");
+	timer.Stop("Manual_PurgeStart");
+end
+
+-- Local Timer Functions
+local function SetPurgeTimer()
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function SetPurgeTimer() :: Called.\n");
+	end
 	
-	-- Let's make the purge end timer!
-	PURGE.EndTimer = CurTime()+settings.fPurgeTime*60
-	net.Start("PurgeEndTimer")
-	net.WriteUInt(PURGE.EndTimer,32)
-	net.Broadcast()
+	PURGE.SetNight(false);
+	bTakeDamage = false;
+	PURGE.Timer = CurTime() + (settings.fPurgeSetupTime * 60);
+	
+	net.Start("PurgeTimer");
+	net.WriteUInt(PURGE.Timer,32);
+	net.Broadcast();
+	BroadcastLua([[chat.AddText(Color(200,72,72),"[ALERT] ",Color(255,255,255),"The Purge starts in 45 minutes!")]]);
+	bPurgeInProgress = false;
 end
 
 local function PurgeAnnouncement()
-	-- Play the announcement!
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function PurgeAnnouncement() :: Called.\n");
+	end
+
 	for k, ply in pairs( player.GetAll() ) do
-		ply:EmitSound("weapons/purge/purge.mp3")
+		ply:EmitSound("weapons/purge/purge.mp3");
+	end
+	
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function PurgeAnnouncement() :: Ended.\n");
+	end
+end
+
+local function PurgeStart()
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function PurgeStart() :: Called.\n");
+	end
+	
+	umsg.Start("PurgeStart");
+	umsg.End();
+	PURGE.SetNight(true);
+	PURGE.Timer = nil;
+	bTakeDamage = true;
+	bPurgeInProgress = true;
+	
+	-- Let's make the purge end timer!
+	PURGE.EndTimer = CurTime() + (settings.fPurgeTime * 60);
+	net.Start("PurgeEndTimer");
+	net.WriteUInt(PURGE.EndTimer,32);
+	net.Broadcast();
+
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function PurgeStart() :: Ended.\n");
 	end
 end
 
 local function PurgeOver()
-	umsg.Start("PurgeOver")
-	umsg.End()
-	for k, ply in pairs( player.GetAll() ) do
-		ply:EmitSound("weapons/purge/purge_end.mp3")
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function PurgeOver() :: Called.\n");
 	end
-	PURGE.SetNight(false)
-	PURGE.EndTimer = nil
-	bPurgeInProgress = false
-	-- Add the rounds up
-	iRounds = iRounds + 1
+	
+	umsg.Start("PurgeOver");
+	umsg.End();
+	
+	for k, ply in pairs( player.GetAll() ) do
+		ply:EmitSound("weapons/purge/purge_end.mp3");
+	end
+	
+	PURGE.SetNight(false);
+	PURGE.EndTimer = nil;
+	bPurgeInProgress = false;
+	
+	iRounds = iRounds + 1;
 	if (iRounds >= settings.iPurgeMaxRounds) then
-		-- Change the map.
+		if (settings.bDebug) then
+			print("[PT DEBUG]Maximum rounds :: Exceeded.\n");
+		end
+		-- Restart the map.
 		for _,v in pairs(player.GetAll()) do
-			v:ChatPrint("Restarting the map in 5 seconds! This is to clear the model precache!")
-			v:ChatPrint("Restarting the map in 5 seconds! This is to clear the model precache!")
-			v:ChatPrint("Restarting the map in 5 seconds! This is to clear the model precache!")
+			v:ChatPrint("Restarting the map in 5 seconds! This is to clear the model precache!");
+			v:ChatPrint("Restarting the map in 5 seconds! This is to clear the model precache!");
+			v:ChatPrint("Restarting the map in 5 seconds! This is to clear the model precache!");
 		end
 		timer.Simple(5, function()
-			RunConsoleCommand("changelevel", game.GetMap())
+			RunConsoleCommand("changelevel", game.GetMap());
 		end)
 	end
 	
+	bPurgeInProgress = false;
+	
 	-- Stop all the timers just in-case.
-	timer.Stop("Timer_SetPurgeTimer");
-	timer.Stop("Timer_PurgeStart");
-	timer.Stop("Timer_PurgeAnnouncement");
-	timer.Stop("Timer_PurgeOver");
-	timer.Stop("Manual_PurgeStart");
-	bPurgeInProgress = false
+	KillTimers();
 	
 	-- Start the timers.
-	timer.Create("Timer_SetPurgeTimer", 1, 1, SetPurgeTimer)
-	timer.Create("Timer_PurgeStart", settings.fPurgeSetupTime * 60, 1, PurgeStart)
-	timer.Create("Timer_PurgeAnnouncement", settings.fPurgeSetupTime * 60 - settings.fPurgeAnnouncementTime, 1, PurgeAnnouncement)
-	timer.Create("Timer_PurgeOver", settings.fPurgeSetupTime * 60 + settings.fPurgeTime * 60, 1, PurgeOver) -- 720 = 12 minutes
+	timer.Create("Timer_SetPurgeTimer", 1, 1, SetPurgeTimer);
+	timer.Create("Timer_PurgeAnnouncement", (settings.fPurgeSetupTime * 60) - settings.fPurgeAnnouncementTime, 1, PurgeAnnouncement);
+	timer.Create("Timer_PurgeStart", settings.fPurgeSetupTime * 60, 1, PurgeStart);
+	timer.Create("Timer_PurgeOver", (settings.fPurgeSetupTime * 60) + (settings.fPurgeTime * 60), 1, PurgeOver);
+	
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function PurgeOver() :: Ended.\n");
+	end
 end
 
--- Global functions used for rounds.
 local function GB_SetUpPurge()
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function GB_SetUpPurge() :: Called.\n");
+	end
+	
+	bPurgeInProgress = false;
+	
 	-- Stop all the timers just in-case.
-	timer.Stop("Timer_SetPurgeTimer");
-	timer.Stop("Timer_PurgeStart");
-	timer.Stop("Timer_PurgeAnnouncement");
-	timer.Stop("Timer_PurgeOver");
-	timer.Stop("Manual_PurgeStart");
-	bPurgeInProgress = false
+	KillTimers();
+	
+	-- Debug
+	if (settings.bDebug) then
+		print("[PT DEBUG]GB_SetUpPurge Timer Values: \n1. " .. settings.fPurgeSetupTime .. "\n");
+	end
 	
 	-- Start the timers.
-	timer.Create("Timer_SetPurgeTimer", 1, 1, SetPurgeTimer)
-	timer.Create("Timer_PurgeStart", settings.fPurgeSetupTime * 60, 1, PurgeStart)
-	timer.Create("Timer_PurgeAnnouncement", settings.fPurgeSetupTime * 60 - settings.fPurgeAnnouncementTime, 1, PurgeAnnouncement)
-	timer.Create("Timer_PurgeOver", settings.fPurgeSetupTime * 60 + settings.fPurgeTime * 60, 1, PurgeOver) -- 720 = 12 minutes
+	timer.Create("Timer_SetPurgeTimer", 1, 1, SetPurgeTimer);
+	timer.Create("Timer_PurgeAnnouncement", (settings.fPurgeSetupTime * 60) - settings.fPurgeAnnouncementTime, 1, PurgeAnnouncement);
+	timer.Create("Timer_PurgeStart", settings.fPurgeSetupTime * 60, 1, PurgeStart);
+	timer.Create("Timer_PurgeOver", (settings.fPurgeSetupTime * 60) + (settings.fPurgeTime * 60), 1, PurgeOver);
+	
+	if (settings.bDebug) then
+		print("[PT DEBUG]Function GB_SetUpPurge() :: Ended.\n");
+	end
 end
 
+-- Hooks
 hook.Add("InitPostEntity","SetupPurge",function()
-	GB_SetUpPurge()
+	if (settings.bDebug) then
+		print("[PT DEBUG]Hook SetupPurge :: Called.\n");
+	end
+	
+	timer.Create("Timer_DelayPurgeStartup", 5.0, 1, function()
+		GB_SetUpPurge();
+	end);
 end)
 
-hook.Add("EntitybTakeDamage", "vehicledamage", function(target, damageinfo)
-	local attacker = damageinfo:GetAttacker()
+hook.Add("EntityTakeDamage", "SetVehicleDamage", function(target, damageinfo)
+	local attacker = damageinfo:GetAttacker();
 	if target:IsPlayer() and (attacker:IsVehicle() or (bit.band(damageinfo:GetDamageType(), DMG_VEHICLE) != 0)) and not bPurgeInProgress then
-		-- So block vehicle damage when Purge isn't going on!
-		damageinfo:ScaleDamage(0)
+		-- Block vehicle damage when the Purge isn't active.
+		damageinfo:ScaleDamage(0);
 	end
 end)
 
-hook.Add("PlayerInitialSpawn","SendPurge",function(ply)
+hook.Add("PlayerInitialSpawn","SendPurge", function (ply)
+	if (settings.bDebug) then
+		print("[PT DEBUG]Hook SendPurge :: Called.\n");
+	end
+	
 	if(PURGE.Timer) then
-		net.Start("PurgeTimer")
-		net.WriteUInt(PURGE.Timer,32)
-		net.Send(ply)
+		net.Start("PurgeTimer");
+		net.WriteUInt(PURGE.Timer,32);
+		net.Send(ply);
+		
+		if (settings.bDebug and ply) then
+			print("[PT DEBUG]Sent PurgeTimer Network :: " .. ply:Nick() .. "\n");
+		end
 	elseif (PURGE.EndTimer) then
-		net.Start("PurgeEndTimer")
-		net.WriteUInt(PURGE.EndTimer,32)
-		net.Send(ply)
-	end
-end)
-
-function PURGE.SetNight(night)
-	if(night) then
-		-- Set the lighting
-		local lightenvs = ents.FindByClass("light_environment")
-		for _, light in pairs(lightenvs) do
-			if (IsValid(light)) then
-				 light:Fire( 'FadeToPattern' , 'a' , 0 ) // Makes the light fully dark, z is fully bright.
-			end
-		end
-	else
-		-- Set the lighting
-		local lightenvs = ents.FindByClass("light_environment")
-		for _, light in pairs(lightenvs) do
-			if (IsValid(light)) then
-				 light:Fire( 'FadeToPattern' , 'v' , 0 ) // Makes the light fully dark, z is fully bright.
-			end
+		net.Start("PurgeEndTimer");
+		net.WriteUInt(PURGE.EndTimer,32);
+		net.Send(ply);
+	
+		if (settings.bDebug and ply) then
+			print("[PT DEBUG]Sent PurgeEndTimer Network :: " .. ply:Nick() .. "\n");
 		end
 	end
-end
-
--- Manual Commands
-concommand.Add("start_purge", function (ply)
-	if not VerifyUser(ply) then
-		ply:ChatPrint("[GFL]Only users on the whitelist can execute this command.")
-		return
-	end
-	
-	-- Start the Purge!
-	
-	-- Checks
-	timer.Stop("Timer_PurgeStart")
-	timer.Stop("Timer_PurgeOver")
-	timer.Stop("Timer_PurgeAnnouncement")
-	
-	PurgeStart()
-	timer.Create("Timer_PurgeOver", settings.fPurgeTime * 60, 1, PurgeOver) -- 720 = 12 minutes
-	
 end)
 
-local function Manual_PurgeStart()
-	-- Start the Purge!
+concommand.Add("purge_start", function (ply)
+	if not VerifyUser(ply) then
+		ply:ChatPrint("[GFL]Only users on the whitelist can execute this command.");
+		return;
+	end
 	
-	-- Checks
-	timer.Stop("Timer_PurgeStart")
-	timer.Stop("Timer_PurgeOver")
-	timer.Stop("Timer_PurgeAnnouncement")
+	if (settings.bDebug and ply) then
+		print("[PT DEBUG]Concommand purge_start :: Called by " .. ply:Nick() .. " (" .. ply:SteamID() .. ").\n");
+	end
 	
-	PurgeStart()
-	timer.Create("Timer_PurgeOver", settings.fPurgeTime * 60, 1, PurgeOver) -- 720 = 12 minutes
-end
+	-- Check
+	KillTimers();
+	
+	if (settings.bDebug) then
+		print("[PT DEBUG]Manually starting Purge.\n");
+	end
+	
+	PurgeStart();
+	timer.Create("Timer_PurgeOver", settings.fPurgeTime * 60, 1, PurgeOver);
+end)
 
-concommand.Add("start_purge2", function (ply)
+concommand.Add("purge_start2", function (ply)
 	if not VerifyUser(ply) then
 		ply:ChatPrint("[GFL]Only users on the whitelist can execute this command.")
 		return
 	end
 	
-	-- Checks
-	timer.Stop("Timer_PurgeStart")
-	timer.Stop("Timer_PurgeOver")
-	timer.Stop("Timer_PurgeAnnouncement")
+	if (settings.bDebug and ply) then
+		print("[PT DEBUG]Concommand purge_start2 :: Called by " .. ply:Nick() .. " (" .. ply:SteamID() .. ").\n");
+	end
 	
-	-- Start the purge with the announcement!
-	PurgeAnnouncement()
-	timer.Create("Manual_PurgeStart", settings.fPurgeAnnouncementTime, 1, Manual_PurgeStart)
+	-- Checks
+	KillTimers();
+	
+	PurgeAnnouncement();
+	timer.Create("Manual_PurgeStart", settings.fPurgeAnnouncementTime, 1, function()
+		if (settings.bDebug) then
+			print("[PT DEBUG]Manually starting Purge (announcement included).\n");
+		end
+		
+		PurgeStart();
+		timer.Create("Timer_PurgeOver", settings.fPurgeTime * 60, 1, PurgeOver);
+	end);
 end)
 
 
-concommand.Add("stop_purge", function (ply)
+concommand.Add("purge_stop", function (ply)
 	if not VerifyUser(ply) then
-		ply:ChatPrint("[GFL]Only users on the whitelist can execute this command.")
-		return
+		ply:ChatPrint("[GFL]Only users on the whitelist can execute this command.");
+		return;
 	end
 	
-	-- End the Purge!
-	PurgeOver()
+	if (settings.bDebug and ply) then
+		print("[PT DEBUG]Concommand purge_stop :: Called by " .. ply:Nick() .. " (" .. ply:SteamID() .. ").\n");
+	end
+	
+	PurgeOver();
 end)
 
 concommand.Add("setup_purge", function (ply)
 	if not VerifyUser(ply) then
-		ply:ChatPrint("[GFL]Only users on the whitelist can execute this command.")
-		return
+		ply:ChatPrint("[GFL]Only users on the whitelist can execute this command.");
+		return;
 	end
 	
-	GB_SetUpPurge()
+	if (settings.bDebug and ply) then
+		print("[PT DEBUG]Concommand setup_purge :: Called by " .. ply:Nick() .. " (" .. ply:SteamID() .. ").\n");
+	end
+	
+	GB_SetUpPurge();
 end)
+
+concommand.Add("purge_status", function (ply)
+	ply:ChatPrint("Timer_PurgeOver: " .. tostring(timer.Exists("Timer_PurgeOver")));
+	ply:ChatPrint("Timer_PurgeStart: " .. tostring(timer.Exists("Timer_PurgeStart")));
+	ply:ChatPrint("Manual_PurgeStart: " .. tostring(timer.Exists("Manual_PurgeStart")));
+	ply:ChatPrint("Timer_PurgeAnnouncement: " .. tostring(timer.Exists("Timer_PurgeAnnouncement")));
+end);
+
+concommand.Add("purge_disable", function (ply)
+	if not VerifyUser(ply) then
+		ply:ChatPrint("[GFL]Only users on the whitelist can execute this command.");
+		return;
+	end
+	
+	KillTimers();
+	ply:ChatPrint("[PT]Purge is now disabled.");
+	
+	if (settings.bDebug and ply) then
+		print("[PT DEBUG]Concommand purge_disable :: Called by " .. ply:Nick() .. " (" .. ply:SteamID() .. ").\n");
+	end
+end)
+
+if settings.bCheck then
+	timer.Create("Purge_Check", 30.0, 0, function()
+		if not timer.Exists("Timer_PurgeOver") and not timer.Exists("Timer_PurgeStart") and not timer.Exists("Manual_PurgeStart") and not timer.Exists("Timer_PurgeAnnouncement") then
+			if (settings.bDebug) then
+				print("[PT DEBUG]Check :: Found no Purge timers. Restarting game-mode.\n");
+			end
+			
+			-- Nothing is happening. Therefore, we must reset the game-mode.
+			GB_SetUpPurge();
+		end
+	end)
+else
+	if (timer.Exists("Purge_Check")) then
+		timer.Stop("Purge_Check");
+	end
+end
